8ab8d5b43380b338439852965d481c15419f0b51
Malog792 <116230295+Malog792@users.noreply.github.com>
2022-10-20 00:53:37 +0000

    Aprendi variáveis e a exibi-lás

--- main.py
+++ main.py
@@ -3,3 +3,8 @@
 # print() = comando de saída
 print("Olá Mundo!")
 print("Caraca vei funcionou, que emoção")
+nome = "Mauricio Lourenço de Aguiar"
+print("Meu nome é:",nome)
+idade = 19
+print("minha idade é:",idade)
+# Exibe nome e idade na tela
