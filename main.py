d9f8996710d08c14dd880096db527b85ba539b56
Malog792 <116230295+Malog792@users.noreply.github.com>
2022-10-26 23:31:35 +0000

    Adicionei uma condição para determinar se é permitido beber ou dirigir ou não é. além do dobro da idade informada

--- main.py
+++ main.py
@@ -3,9 +3,20 @@
 nome = input("Digite seu nome: ")
 
 idade = int(input("Digite a sua idade: "))
+dobro= idade*2
 # exiba a sua idade é ..
-idade = idade+40
+#idade = idade+40
 print ("Seu nome é: "+nome)
 print(f"A sua idade é:{idade} ")
+print(f"O dobro da sua idade é: {dobro}")
 
 # comando de saída..exibir na tela
+
+#Estrutura condicional - o if
+# Se a pessoa for maior de idade, mostre 
+#"Você é maior de idade, ótimo"
+
+if idade >= 18:
+  print ("Você é maior de idade, ótimo, pode beber ou dirigir")
+else:
+  print("Fique em casa, você deveria estar estudando")
