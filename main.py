a7b9d202de5ed9bf6414fe9f6748ce5a855d4577
Malog792 <116230295+Malog792@users.noreply.github.com>
2022-10-27 00:33:26 +0000

    Coloquei comparativos

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # comando input(): quero permitir que o usuario digite algo
 
-nome = input("Digite seu nome: ")
+nome = input("Digite seu nome: \n")
 
-idade = int(input("Digite a sua idade: "))
+idade = int(input("Digite a sua idade: \n"))
+genero = input("Digite M para genero masculino e F para feminino: \n")
 dobro= idade*2
 # exiba a sua idade é ..
 #idade = idade+40
 print ("Seu nome é: "+nome)
+print ("Seu Gênero é: "+genero)
 print(f"A sua idade é:{idade} ")
 print(f"O dobro da sua idade é: {dobro}")
 
@@ -16,7 +18,16 @@ print(f"O dobro da sua idade é: {dobro}")
 # Se a pessoa for maior de idade, mostre 
 #"Você é maior de idade, ótimo"
 
-if idade >= 18:
-  print ("Você é maior de idade, ótimo, pode beber ou dirigir")
+if idade >= 18 and genero == "M" or "m":
+  print ("Você é maior de idade, ótimo, pode beber ou dirigir \n")
+ 
+  print("Você precisa/precisou prestar serviço militar obrigatório")
+
+  
 else:
   print("Fique em casa, você deveria estar estudando")
+  
+# E se eu quisesse perguntar o gênero (M = masculino e o F = feminino)
+# Mostre (.. e você também precisa/precisou prestar o serviço militar obrigatório)
+
+
