ba85b3a786b26d3086c57261a99dde155ba1356b
Malog792 <116230295+Malog792@users.noreply.github.com>
2022-11-10 01:02:24 +0000

    Funções criadas e laço de repetição com calculo de impostos

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ print(imposto2)
 
 #Criar uma função calcular_imposto() que calcula um imposto de 5% e retorna a quem pediu...
 def calcular_imposto(preco_produto):
-  imposto = preco_produto * 0.05
+  imposto = preco_produto * 0.07
   return imposto
 
 # Aqui é o uso ... aqui é imposto a calcular..
@@ -24,4 +24,8 @@ imposto = calcular_imposto(preco)
 print(imposto)
 
 
-  
+valores = [1.99, 24.50, 78.27, 1515.5] 
+# Se eu quiser calcular o imposto destes quatro valores... e exibir na tela assim: "O imposto de .... é ..."(1º preço, 2º. imposto)
+
+for valor in valores:
+  print(f"O imposto de {valor} é {calcular_imposto(valor)}")
