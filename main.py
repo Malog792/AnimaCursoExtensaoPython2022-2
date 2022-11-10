ddfdec05ca740732336bbfb40b3746728c949a30
Malog792 <116230295+Malog792@users.noreply.github.com>
2022-11-10 00:36:05 +0000

    Declaração de funções

--- main.py
+++ main.py
@@ -1,45 +1,27 @@
-print("Início da aula 3")
-contador = 1
+#criação de funções
 
-# Exibir de 1 a 10 repetidamente
-while(contador <= 10):
-  print(contador)
-  contador = contador+1 #contador += 1
+preco = 1999.90
+
+#calcular 5% de imposto. guadar na variavel imposto e exibir na tela
+
+imposto = preco*0.05
+preco = imposto + preco
+print(imposto)
+print(f"{preco}\n")
+
+preco2 = 100
+imposto2 = preco2 * 0.05
+print(imposto2)
+
+#Criar uma função calcular_imposto() que calcula um imposto de 5% e retorna a quem pediu...
+def calcular_imposto(preco_produto):
+  imposto = preco_produto * 0.05
+  return imposto
+
+# Aqui é o uso ... aqui é imposto a calcular..
+preco = 299
+imposto = calcular_imposto(preco)
+print(imposto)
 
 
-#Laço for (python for= for each)
-fruta = "Morango"
-print(fruta)
-
-fruta1 = "morango"
-fruta2 = "laranja"
-fruta3 = "pêra"
-
-#Lista
-frutas = ["morango","laranja","pêra"]
-
-#mostra todas
-print(frutas)
-
-#quero exibir apenas a 3ªfruta(pêra)
-print(frutas[2])
-
-#Quantas frutas tem na minha lista?
-print(len(frutas)) # length = tamanho
-
-#Quero incluir uma fruta nova
-frutas.append("manga")
-frutas.append("Banana")
-
-print(len(frutas))
-print(frutas)
-
-i=0
-while(i<len(frutas)):
-  print(frutas[i])
-  i= i + 1
-
-print("Exemplo das frutas com FOR:\n")
-
-for fruta in frutas:#Aqui não pode ter parênteses
-  print(fruta)
+  
